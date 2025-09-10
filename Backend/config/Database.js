dbeebec8f9e71735d7b8ffeb48eaaa4b7754a5fa
irsyadkk <irsyadkhairullah7@gmail.com>
2025-09-11 03:27:01 +0700

    fix

--- Backend/config/Database.js
+++ Backend/config/Database.js
@@ -5,16 +5,13 @@ dotenv.config();
 const DB_NAME = process.env.DB_NAME;
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-// const DB_HOST = process.env.DB_HOST;
+const DB_HOST = process.env.DB_HOST;
 // const DB_PORT = process.env.DB_PORT;
-const DB_SOCKET_PATH = process.env.DB_HOST;
 
 const db = new Sequelize(DB_NAME, DB_USERNAME, DB_PASSWORD, {
   dialect: "postgres",
   logging: false,
-  dialectOptions: {
-    host: DB_SOCKET_PATH,
-  },
+  host: DB_HOST,
 });
 
 export default db;
