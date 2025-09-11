6e7f59f475bdf264ae750cbd3f6572ce9a2e27d9
irsyadkk <irsyadkhairullah7@gmail.com>
2025-09-12 02:40:12 +0700

    fix

--- pgbouncer/entrypoint.sh
+++ pgbouncer/entrypoint.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 set -e
 
-# Ganti template dengan nilai environment variable
-envsubst < /etc/pgbouncer/pgbouncer.ini.template > /etc/pgbouncer/pgbouncer.ini
+# Buat direktori writable untuk runtime config
+mkdir -p /home/pgbouncer/config
+envsubst < /etc/pgbouncer/pgbouncer.ini.template > /home/pgbouncer/config/pgbouncer.ini
 
-# Jalankan PgBouncer
-exec pgbouncer -vv /etc/pgbouncer/pgbouncer.ini
+# Jalankan PgBouncer dengan file yang baru
+exec pgbouncer -vv /home/pgbouncer/config/pgbouncer.ini
