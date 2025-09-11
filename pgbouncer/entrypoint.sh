d71fe1f6c9ebf33abe3f1c060b0c9464a70a4f9c
irsyadkk <irsyadkhairullah7@gmail.com>
2025-09-12 02:24:29 +0700

    fix

---
+++ pgbouncer/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+
+# Ganti template dengan nilai environment variable
+envsubst < /etc/pgbouncer/pgbouncer.ini.template > /etc/pgbouncer/pgbouncer.ini
+
+# Jalankan PgBouncer
+exec pgbouncer -vv /etc/pgbouncer/pgbouncer.ini
+chmod +x entrypoint.sh
