029c608d3690e3d98edc1f841fd767a6aecb8090
Arjikusna M <arjiduba@gmail.com>
2025-09-11 18:24:03 +0700

    fix addmember

--- Frontend/src/utils.js
+++ Frontend/src/utils.js
@@ -1,4 +1,4 @@
-// export const BASE_URL = "http://localhost:5000";
+export const BASE_URL = "http://localhost:5000";
 // export const BASE_URL = "https://api-hut-kai-80-321462847441.asia-south2.run.app";
-export const BASE_URL = process.env.REACT_APP_API_URL || "https://api-hut-kai-80-321462847441.asia-south2.run.app";
+// export const BASE_URL = process.env.REACT_APP_API_URL || "https://api-hut-kai-80-321462847441.asia-south2.run.app";
 export const ADMIN_NIPP = process.env.REACT_APP_ADMIN_NIPP;
